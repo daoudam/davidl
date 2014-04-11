f695e27c6e8b1fb0aeba1cc40e41f269b9fd862e
David Leuliette <dleuliette@siliconsalad.com>
2014-04-11 08:57:09 +0200

    Site updated to 4ccdff0

---
+++ assets/javascripts/blog-7c9ec3d7.js
@@ -0,0 +1,1 @@
+(function(){$(".js-mailchimp-destination").click(function(){return $("#js-mailchimpemail").focus()}),$("#js-sub-nav--home a").click(function(t){t.preventDefault(),$("#js-sub-nav--home dd").removeClass("active"),$(this).parent("dd").addClass("active")}),$(function(){$("#js-mixitup-container").mixItUp()})}).call(this);
