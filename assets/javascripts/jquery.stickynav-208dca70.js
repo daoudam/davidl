0d8ed37c41a8b4fce08b5128279a34199eb9b89e
David Leuliette <dleuliette@gmail.com>
2014-10-19 23:18:41 +0200

    Site updated to 22ccf8a

---
+++ assets/javascripts/jquery.stickynav-208dca70.js
@@ -0,0 +1,1 @@
+(function(){$(function(){var t,e;return t=document.querySelector("#js-topbar"),e=new Headroom(t,{offset:300,tolerance:10,classes:{initial:"animated",pinned:"slideDown",unpinned:"slideUp"}}),e.init()})}).call(this);
