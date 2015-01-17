b7d078959c31d08aae577e17197d224299bd6332
David Leuliette <dleuliette@gmail.com>
2015-01-17 19:22:11 +0100

    Site updated to 055412d

--- assets/javascripts/topbar-5d56397e.js
+++
@@ -1,1 +0,0 @@
-(function(){$(function(){var t,e;return t=document.querySelector("#js-topbar"),e=new Headroom(t,{offset:300,tolerance:10,classes:{initial:"animated",pinned:"headroom--pinned",unpinned:"headroom--unpinned"}}),e.init()})}).call(this);
